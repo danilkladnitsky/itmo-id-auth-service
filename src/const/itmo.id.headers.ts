5b37c2518803386e62f8fd8bd3b7dc24142961bd
Danil Kladnitskiy <daniil.kla@fora-soft.com>
2022-03-16 23:53:05 +0300

    headers: fix HOST address

--- src/const/itmo.id.headers.ts
+++ src/const/itmo.id.headers.ts
@@ -1,14 +1,14 @@
 export const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   Accept: '*/*',
-  Host: 'login.itmo.ru',
+  Host: 'id.itmo.ru',
   'Accept-Encoding': 'gzip, deflate, br',
   Connection: 'keep-alive',
 };
 
 export const headersWithAccessToken = (access_token) => ({
   Accept: '*/*',
-  Host: 'login.itmo.ru',
+  Host: 'id.itmo.ru',
   'Accept-Encoding': 'gzip, deflate, br',
   Connection: 'keep-alive',
   Authorization: `Bearer ${access_token}`,
