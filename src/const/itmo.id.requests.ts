a65934e84165ad6b832e6d57a7c9148e4025f933
Danil Kladnitskiy <daniil.kla@fora-soft.com>
2022-03-17 16:12:21 +0300

    Auth: change url

--- src/const/itmo.id.requests.ts
+++ src/const/itmo.id.requests.ts
@@ -1,5 +1,5 @@
 export const getAccessTokenRequest =
-  'https://login.itmo.ru/auth/realms/itmo/protocol/openid-connect/token';
+  'https://id.itmo.ru/auth/realms/itmo/protocol/openid-connect/token';
 
 export const getUserDataRequest =
-  'https://login.itmo.ru/auth/realms/itmo/protocol/openid-connect/userinfo';
+  'https://id.itmo.ru/auth/realms/itmo/protocol/openid-connect/userinfo';
